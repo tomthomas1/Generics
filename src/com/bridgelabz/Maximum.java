a0b7cf18b3c68232d8d3c460f1ae612698a23d33
tomthomas1 <tomthomas8688@gmail.com>
2022-03-01 01:08:45 +0530

    Given 3 String find the maximum

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -2,15 +2,15 @@ package com.bridgelabz;
 
 public class Maximum {
 	
-	//method to check the maximum float number
-		public void floatMax(Float first, Float second, Float third) {
-			Float max = first;                                  //considering first is max and assigning max to first
+	//method to check the maximum String. In string it will check the alphabetic order weightage.
+		public void StringMax(String first, String second, String third) {
+			String max = first;                                  //considering first is max and assigning max to first
 			 String position = "first";                         //for displaying the position
 			if (second.compareTo(max) > 0) {	
 				max = second;                                   //comparing with first max and if greater that assigning to max
 				position = "second";
 			}
-			 if (third.compareTo(max) > 0) {                    //comparing with max float and if greater that assigning to max
+			 if (third.compareTo(max) > 0) {                    //comparing with max string and if greater that assigning to max
 				max = third;
 				position = "third";
 			}  System.out.println("The maximum number is in " + position + " position and the number is : "   + max);
--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -8,12 +8,12 @@ public class TestMax {
 		Maximum max = new Maximum();                              //creating object
 
 		System.out.println("Please enter the first number: ");          //taking input from user
-		Float first = sc.nextFloat();
+		String first = sc.next();
 		System.out.println("Please enter the second number: ");
-		Float second = sc.nextFloat();
+		String second = sc.next();
 		System.out.println("Please enter the third number: ");
-		Float third = sc.nextFloat();
-		max.floatMax(first, second, third);                             //passing the values to the method to compute the max Float
+		String third = sc.next();
+		max.StringMax(first, second, third);                             //passing the values to the method to compute the max String
 
 	}
 
