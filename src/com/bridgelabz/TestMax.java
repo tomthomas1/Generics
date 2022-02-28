5b533a03d94215914d5df360987f5727fc6c2da4
tomthomas1 <tomthomas8688@gmail.com>
2022-03-01 01:00:17 +0530

    Given 3 Floats find the maximum

--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class TestMax {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		MaxInteger max = new MaxInteger();                              //creating object
+		Maximum max = new Maximum();                              //creating object
 
 		System.out.println("Please enter the first number: ");          //taking input from user
-		Integer first = sc.nextInt();
+		Float first = sc.nextFloat();
 		System.out.println("Please enter the second number: ");
-		Integer second = sc.nextInt();
+		Float second = sc.nextFloat();
 		System.out.println("Please enter the third number: ");
-		Integer third = sc.nextInt();
-		max.IntMax(first, second, third);                              //passing the values to the method to compute the max Integer
+		Float third = sc.nextFloat();
+		max.floatMax(first, second, third);                             //passing the values to the method to compute the max Float
 
 	}
 
