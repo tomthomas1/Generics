dbb84a6f4eb7bd35b3fad5f576c13501d88398ef
tomthomas1 <tomthomas8688@gmail.com>
2022-03-01 01:39:42 +0530

    One Generic method to find max

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,16 +1,15 @@
 package com.bridgelabz;
 
 public class Maximum {
-	
-	//method to check the maximum String. In string it will check the alphabetic order weightage.
-		public void StringMax(String first, String second, String third) {
-			String max = first;                                  //considering first is max and assigning max to first
+
+	public <T extends Comparable<T>> void GenMax(T first, T second, T third) {     //using generic type T and making the method generic.
+			T max = first;                                      //considering first is max and assigning max to first
 			 String position = "first";                         //for displaying the position
 			if (second.compareTo(max) > 0) {	
 				max = second;                                   //comparing with first max and if greater that assigning to max
 				position = "second";
 			}
-			 if (third.compareTo(max) > 0) {                    //comparing with max string and if greater that assigning to max
+			 if (third.compareTo(max) > 0) {                    //comparing with max value and if greater that assigning to max
 				max = third;
 				position = "third";
 			}  System.out.println("The maximum number is in " + position + " position and the number is : "   + max);
--- src/com/bridgelabz/TestMax.java
+++ src/com/bridgelabz/TestMax.java
@@ -3,18 +3,66 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TestMax {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Maximum max = new Maximum();                              //creating object
-
-		System.out.println("Please enter the first number: ");          //taking input from user
-		String first = sc.next();
-		System.out.println("Please enter the second number: ");
-		String second = sc.next();
-		System.out.println("Please enter the third number: ");
-		String third = sc.next();
-		max.StringMax(first, second, third);                             //passing the values to the method to compute the max String
-
+	public static void main(String[] args) {                                                  //main method to find maximum
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		Maximum max = new Maximum();
+		System.out.println("---------Find Maximum-------- ");
+		
+		while(true) {                                                                         // asking user to select the generic type
+			System.out.println(" \n 1. Integers\n 2. Float\n 3. String\n 4. Exit");
+			System.out.print(" Please enter your choise from above:  \n ");
+			int opt = scan.nextInt();
+			
+			switch(opt) {                                                                     //taking input from user
+			case 1:
+			{
+				System.out.print(" Please enter the first Integer value: ");
+				Integer first = scan.nextInt();
+				
+				System.out.print(" Please enter the second Integer value: ");
+				Integer second = scan.nextInt();
+				
+				System.out.print(" Please enter the third Integer value: ");
+				Integer third = scan.nextInt();
+				max.GenMax(first, second, third);
+				break;
+			}				
+			case 2:
+			{
+				System.out.print(" Please enter the first float value: ");
+				Float first = scan.nextFloat();
+				
+				System.out.print(" Please enter the second float value: ");
+				Float second = scan.nextFloat();
+				
+				System.out.print(" Please enter the third float value: ");
+				Float third = scan.nextFloat();
+				max.GenMax(first, second, third);
+				break;
+			}
+			case 3:
+			{
+				System.out.print(" Please enter the first string : ");
+				String first = scan.next();
+				
+				System.out.print(" Please enter the second string: ");
+				String second = scan.next();
+				
+				System.out.print(" Please enter the third string: ");
+				String third = scan.next();
+				max.GenMax(first, second, third);
+				break;
+			}
+			case 4:
+				System.out.println(" You have exited from the program!!");
+				scan.close();
+				return;
+			default:
+				System.out.println("Please enter a valid input from above");				
+			}
+				
+		}
 	}
 
 }
